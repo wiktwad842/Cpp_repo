206993c4dc541f6a3e418ca1c46c87d4f099e444
Wiktor Wadas <wiktwad842@student.polsl.pl>
2020-10-12 13:06:05 +0200

    1

--- udemy_cpp/Classes and Objects/AccessingClassMembers/AccessingClassMembers/Source.cpp
+++ udemy_cpp/Classes and Objects/AccessingClassMembers/AccessingClassMembers/Source.cpp
@@ -4,42 +4,58 @@ using namespace std;
 
 class Player {
 	//attributes
-	string name{ "Player" };
-	int health{ 100 };
-	int xp{ 3 };
+public:
+	string name;
+	int health;
+	int xp;
 	//methods
-	void talk(string);
+	void talk(string text_to_say) {
+		cout << name << " says " << text_to_say << endl;
+	}
 	bool isDead();
 
 };
 class Account {
 	//attributes
-	string name{ "Account" };
-	double balance{ 0.0 };
+public:
+	string name;
+	double balance;
 	//methods
-	bool deposit(double);
-	bool withdraw(double);
+	bool deposit(double bal) {
+		balance += bal;
+		cout << "In deposit" << endl;
+		return true;
+	}
+	bool withdraw(double bal) {
+		balance -= bal;
+		cout << "In withdrawal" << endl;
+		return true;
+	}
 };
-
 
 int main() {
 
 	Account frank_account;
-	Account jim_account;
+	
+	frank_account.name = "Frank's account";
+	frank_account.balance = 5000.0;
+
+	frank_account.deposit(1000.0);
+	frank_account.withdraw(500.0);
 
 	Player frank;
 	Player hero;
 
-
-
-	Player players[]{ frank,hero };
-
-	vector<Player> player_vec{ frank };
-	player_vec.push_back(hero);
-
-	Player* enemy{ nullptr };
-	enemy = new Player;
-	delete enemy;
+	frank.name = "Frank";
+	frank.health = 100;
+	frank.xp = 12;
+	frank.talk("Hi there");
+	
+	Player* enemy = new Player;
+	(*enemy).name = "Enemy";
+	(*enemy).health = 100;
+	enemy->xp = 15;
+	enemy->talk("I will destroy you");
 
 
 	return 0;
